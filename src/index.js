95877dc0c39da5ef4b377e738d48b01ff14ab176
Prashant Choudhary <prashant.srs@gmail.com>
2018-12-14 16:45:02 +0530

    Players Can Play trun now

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<div class="flex-container">
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
+const game = {
+  players: ["X", "O"],
+  currentPlayer: "X",
+  boxes: ["", "", "", "", "", "", "", "", ""]
+};
+
+const toggleCurrentPlayer = () => {
+  const { players, currentPlayer } = game;
+  game.currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+};
+
+const onBoxClick = (item, i) => {
+  if (!item) {
+    const { currentPlayer } = game;
+    game.boxes[i] = currentPlayer;
+    toggleCurrentPlayer();
+    renderBaseBoard(game);
+  }
+};
+const renderBaseBoard = game => {
+  const { boxes, currentPlayer } = game;
+  const App = document.getElementById("app");
+  App.innerHTML = `
+<div class="container">
 </div>
 `;
+  const container = document.getElementsByClassName("container")[0];
+  boxes.forEach((item, i) => {
+    const box = document.createElement("div");
+    box.innerHTML = item;
+    box.id = `box${i}`;
+    box.addEventListener("click", () => onBoxClick(item, i));
+    container.appendChild(box);
+  });
+  const statusBar = document.createElement("div");
+  statusBar.id = "statusBar";
+  statusBar.innerHTML = `Current Player: ${currentPlayer}`;
+  App.appendChild(statusBar);
+};
+
+renderBaseBoard(game);
